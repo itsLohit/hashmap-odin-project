841345ae37cd3f10b6cda74e357e64e166db5718
itsLohit <109834876+itsLohit@users.noreply.github.com>
2025-05-17 16:32:43 +0530

    Completed the HashMap Project

--- hashMap.js
+++ hashMap.js
@@ -49,38 +49,114 @@ class HashMap {
     }
 
     get(key) {
-
+        let index = this.hash(key);
+        let listIndex = this.buckets[index].find(key);
+        if (this.buckets[index] !== undefined && this.buckets[index].contains(key) !== false) {
+            return this.buckets[index].at(listIndex);
+        }
+        else{
+            return null;
+        }
     }
 
     has(key) {
-
+        if(this.get(key)){
+            return true;
+        }
+        else {
+            return false;
+        }
     }
 
     remove(key) {
-
+        let index = this.hash(key);
+        let listIndex = this.buckets[index].find(key);
+        if (this.buckets[index] !== undefined && this.buckets[index].contains(key) !== false) {
+            this.buckets[index].removeAt(listIndex);
+            return true;
+        }
+        else{
+            return false;
+        }
     }
 
     length() {
-
+        let length = 0;
+        for (let bucket of this.buckets) {
+            if (bucket === undefined) {
+                continue;
+            } 
+            else {
+                length += bucket.size();
+            }
+        }
+        return length;
     }
 
     clear() {
-
+        this.buckets = new Array(this.capacity);
     }
 
     keys() {
-
+        let keysArray = [];
+        for (let bucket of this.buckets) {
+            if (bucket === undefined) {
+                continue;
+            } 
+            else {
+                let current = bucket.head;
+                while(current !== null) {
+                    keysArray.push(current.key);
+                    current = current.nextNode;
+                }
+            }
+        }
+        return keysArray;
     }
 
     values() {
+        let valuesArray = [];
+        for (let bucket of this.buckets) {
+            if (bucket === undefined) {
+                continue;
+            } 
+            else {
+                let current = bucket.head;
+                while(current !== null) {
+                    valuesArray.push(current.value);
+                    current = current.nextNode;
+                }
+            }
+        }
+        return valuesArray;
 
     }
 
     entries() {
-        
+        let entriesArray = [];
+        for (let bucket of this.buckets) {
+            if (bucket === undefined) {
+                continue;
+            } 
+            else {
+                let current = bucket.head;
+                while(current !== null) {
+                    entriesArray.push([current.key, current.value]);
+                    current = current.nextNode;
+                }
+            }
+        }
+        return entriesArray;
     }
 
     grow() {
-        
-    }
+        let entries = this.entries();
+        this.capacity = this.capacity * 2;
+        this.buckets = new Array(this.capacity);
+        entries.forEach(entry => {
+            this.set(entry[0], entry[1]);
+        });
+      }
 }
+
+export {HashMap};
